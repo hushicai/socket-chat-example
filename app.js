0bd1417998cfc75ebca74ebabea2b34db1ac6c59
hushicai <bluthcy@gmail.com>
2018-06-15 22:32:41 +0800

    feat: add chat logic

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-(function () {
-    let socket = io();
-})();
+$(function () {
+    var socket = io();
+
+    $('form').submit(function(){
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+
+        return false;
+    });
+
+    socket.on('chat message', function(msg){
+        $('#messages').append($('<li>').text(msg));
+    });
+});
--- server.js
+++ server.js
@@ -2,14 +2,25 @@ var app = require('express')();
 var serveStatic = require('serve-static')
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var debug = require('debug')('socket.io-example');
 
 app.use(serveStatic(__dirname, {index: ['index.html']}));
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    debug('a user connected');
+
+    socket.on('disconnect', function(){
+        debug('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        debug('message: ' + msg);
+
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    debug('listening on *:3000');
 });
-    
+
